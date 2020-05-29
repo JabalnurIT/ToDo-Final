af53ea9c880895f3f62a927c0de4f7b1e3d76663
JabalnurIT <jabalnur.it@gmail.com>
2020-05-29 18:11:33 +0800

    Menambahkan AddActivity dan UpdateActivity

---
+++ app/src/main/java/com/finalmobile/todo/UpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.finalmobile.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
