1d235874bd92af85db9e9fd8d45e69e1c8e6b9e7
JabalnurIT <jabalnur.it@gmail.com>
2020-05-29 19:07:27 +0800

    Menghubungkan MainActivity dengan AddActivity

--- app/src/main/java/com/finalmobile/todo/activity/MainActivity.kt
+++ app/src/main/java/com/finalmobile/todo/activity/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.finalmobile.todo.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.finalmobile.todo.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var floatingActionButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        floatingActionButton = findViewById(R.id.fab)
+
+        floatingActionButton.setOnClickListener {
+            addList()
+        }
+    }
+
+    private fun addList(){
+        val addIntent = Intent(this, AddActivity::class.java)
+        startActivity(addIntent)
     }
 }
